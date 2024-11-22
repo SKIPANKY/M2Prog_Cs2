95aec7a124e1db29be335b0d5b17b7e73af4a371
SKIPANKY <mathijswijs2007@gmail.com>
2024-11-22 10:52:54 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            program RUN();
+       
         }
+        //hier komt je function
+        internal void Run()
+        {
+            Console.WriteLine("how long do you think you'd survive in a zombie apocalypse?");
+            string.antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+    }
     }
 }
